3887e27051b8797708ff20e3da78ae0b0a530b25
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-19 21:20:48 -0300

    Convertendo data-hora para texto

--- date-time/src/app/Program.java
+++ date-time/src/app/Program.java
@@ -3,6 +3,7 @@ package app;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
@@ -11,6 +12,7 @@ public class Program {
 		
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
 		
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
@@ -31,8 +33,15 @@ public class Program {
 		System.out.println("d02 = " + d02);
 		System.out.println("d03 = " + d03);
 		System.out.println("d04 = " + d04);
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 = " + fmt1.format(d04));
+		System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		System.out.println("d05 = " + d05);
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + fmt2.format(d05));
+		System.out.println("d05 = " + d05.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
 		System.out.println("d06 = " + d06);
+		System.out.println("d06 = " + fmt3.format(d06));
 		System.out.println("d07 = " + d07);
 		System.out.println("d08 = " + d08);
 		System.out.println("d09 = " + d09);
