a956e761b25f690bef3effad6ceb1b8c5c5244e4
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-21 08:54:43 -0300

    Herança

---
+++ ContaBancaria/src/entities/BusinessAccount.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class BusinessAccount extends Account {
+	
+	private Double loanLimit;
+	
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimrit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimrit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(Double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10;
+		}
+		
+	}
+	
+}
