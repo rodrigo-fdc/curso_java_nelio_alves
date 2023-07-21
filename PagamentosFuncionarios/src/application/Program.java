e744e0f2a2bca77c36987161ce7434578c7ab978
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-21 11:47:45 -0300

    Refatoração do Exercício Resolvido - Herança e Polimorfismo de acordo com a correção do vídeo

--- PagamentosFuncionarios/src/application/Program.java
+++ PagamentosFuncionarios/src/application/Program.java
@@ -11,58 +11,49 @@ import entities.OutsourcedEmployee;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		List<Employee> list = new ArrayList<>();
-		
-		
+
 		System.out.print("Enter the numbber employee: ");
 		int n = sc.nextInt();
 		sc.nextLine();
-		
-		
-		
-		for (int i=0; i<n; i++) {
-			System.out.println("Employee #" + (i+1) + " data:");
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Employee #" + (i + 1) + " data:");
 			System.out.print("Outsourced (y/n)? ");
 			char question = sc.next().charAt(0);
 			sc.nextLine();
-			if (question == 'n') {
-				System.out.print("Name: ");
-				String name = sc.nextLine();
-				System.out.print("Hours: ");
-				int hour = sc.nextInt();
-				sc.nextLine();
-				System.out.print("Value per hour: ");
-				double valuePerHour = sc.nextDouble();
-				
-				Employee employee = new Employee(name, hour, valuePerHour);
-				list.add(employee);
-			} else {
-				System.out.print("Name: ");
-				String name = sc.nextLine();
-				System.out.print("Hours: ");
-				int hour = sc.nextInt();
-				sc.nextLine();
-				System.out.print("Value per hour: ");
-				double valuePerHour = sc.nextDouble();
+
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Hours: ");
+			int hour = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			if (question == 'y') {
 				System.out.print("Additional charge: ");
 				double additionalCharge = sc.nextDouble();
-				
-				
+
 				Employee employee = new OutsourcedEmployee(name, hour, valuePerHour, additionalCharge);
+				list.add(employee);
+			} else {
+
+				Employee employee = new Employee(name, hour, valuePerHour);
 				list.add(employee);
 			}
 		}
-		
 
 		System.out.println("PAYMENTS");
-		for(Employee employee : list) {
+		for (
+
+		Employee employee : list) {
 			System.out.println(employee.getName() + " - $ " + String.format("%.2f", employee.payment()));
 		}
-		
+
 		sc.close();
 
 	}
