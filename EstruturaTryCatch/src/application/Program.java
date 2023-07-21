1302ded1d9f7573d685b767727ad9ab0e4abf29a
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-21 17:37:37 -0300

    Pilha de chamadas de métodos (stack trace)

--- EstruturaTryCatch/src/application/Program.java
+++ EstruturaTryCatch/src/application/Program.java
@@ -6,26 +6,45 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+
+		method1();
+		
+		System.out.println("End of Program!");
+
+
+
+	}
+	
+	public static void method1() {
+		System.out.println("***Method1 Start***");
+		
+		
+		method2();
+		
+		
+		System.out.println("***Method1 End***");
+	}
+
+	public static void method2() {
+		
+		System.out.println("***Method2 Start***");
 		
 		Scanner sc = new Scanner(System.in);
-		
+
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
-		}
-		catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid Position");
-		} catch(InputMismatchException e) {
+			e.printStackTrace();
+		} catch (InputMismatchException e) {
 			System.out.println("Input Error");
 		}
 		
-	
-		System.out.println("End of Program!");
-		
-		
-		sc.close();
+		System.out.println("***Method2 End***");
 
+		sc.close();
 	}
 
 }
