655bf968064823392c5c14172555b02842770974
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-21 09:33:31 -0300

    Sobreposição, super e anotação @Override

--- ContaBancaria/src/application/Program.java
+++ ContaBancaria/src/application/Program.java
@@ -8,15 +8,24 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc = new Account(1001, "Alex", 0.0);
+		Account acc = new Account(1001, "Alex", 1000.0);
+		acc.withdraw(200.0);
+		System.out.println(acc.getBalance());
+		
 		
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
 		
 		//UPCASTING
 		
 		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		Account acc2 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
+		
+		Account acc3 = new SavingsAccount(1004, "Anna", 1000.0, 0.01);
+		acc3.withdraw(200.0);
+		System.out.println(acc3.getBalance());
+		
 		
 		//DOWNCASTING
 		
