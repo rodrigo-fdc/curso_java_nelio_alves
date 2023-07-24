3977fe26e911367643f520d6a254cca66fdb084f
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-24 20:28:35 -0300

    Resolução do exercicio sem utilizar interface

---
+++ LocacaoDaCarros/src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.service.BrazilTaxService;
+import model.service.RentalService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Entre com os dados do aluguel");
+		System.out.print("MOdelo do carro: ");
+		String carModel = sc.nextLine();
+		
+		
+		System.out.println("Retirada (DD/MM/YYYY) hh:mm");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.println("Retorno (DD/MM/YYYY) hh:mm");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.print("Entre com preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+
+		System.out.print("Entre com preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("FATURA: ");
+		System.out.println("Pagamento basico: " + cr.getInvoice().getBasicPayment() );
+		System.out.println("Imposto: " + cr.getInvoice().getTax());
+		System.out.println("Pagamento Total: " +cr.getInvoice().getTotalPayment());
+		
+		sc.close();
+	}
+
+}
