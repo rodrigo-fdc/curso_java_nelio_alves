8ebc9af4942e5a87a3a2324e17a10ac5d0acc3d3
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-24 20:37:57 -0300

    Resolução do exercicio utilizando interface

--- LocacaoDaCarros/src/model/service/BrazilTaxService.java
+++ LocacaoDaCarros/src/model/service/BrazilTaxService.java
@@ -1,8 +1,8 @@
 package model.service;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService{
 
-	public Double tax(double amount) {
+	public double tax(double amount) {
 		if (amount <= 100.0) {
 			return amount *0.2;
 		} else {
