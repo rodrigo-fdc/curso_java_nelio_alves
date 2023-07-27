8431592b019fb699ad4eec3992b63349f544dfa1
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-27 16:48:27 -0300

    Hascode e Equals

---
+++ hashcode-equals/src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import entities.Client;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Client c1 = new Client("Maria", "maria@gmail.com.br");
+		Client c2 = new Client("Maria", "maria@gmail.com.br");
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+	}
+
+}
